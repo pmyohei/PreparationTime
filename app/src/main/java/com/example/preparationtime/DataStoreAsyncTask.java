36e9063e4370851dcd1e08ddcc3eac9ee45cb5ef
pmyohei <pmyohei>
2021-07-16 18:43:47 +0900

    Roomの実装 ・操作種別に応じたメソッドを実装（中身は未実装）

--- app/src/main/java/com/example/preparationtime/DataStoreAsyncTask.java
+++ app/src/main/java/com/example/preparationtime/DataStoreAsyncTask.java
@@ -1,6 +1,7 @@
 package com.example.preparationtime;
 
 import android.os.AsyncTask;
+import android.widget.LinearLayout;
 
 /*
  * 非同期-DBアクセスクラス
@@ -15,17 +16,19 @@ public class DataStoreAsyncTask extends AsyncTask<Void, Void, Integer> {
         DELETE;         //削除
     }
 
-    private DB_OPERATION operation;
-    private int p_key;
-    private String task;
-    private int taskTime;
+    private DB_OPERATION    operation;
+    private int             p_key;
+    private String          task;
+    private int             taskTime;
+    private LinearLayout    rootView;
 
     /*
      * コンストラクタ
      *   表示
      */
-    public DataStoreAsyncTask(DB_OPERATION operation){
+    public DataStoreAsyncTask(DB_OPERATION operation, LinearLayout rootView){
         this.operation = operation;
+        this.rootView  = rootView;
     }
 
     /*
@@ -58,19 +61,66 @@ public class DataStoreAsyncTask extends AsyncTask<Void, Void, Integer> {
         this.p_key      = p_key;
     }
 
-
     @Override
     protected Integer doInBackground(Void... params) {
 
-        //操作種別に応じた処理
+        //--操作種別に応じた処理
+        if(this.operation == DB_OPERATION.CREATE){
+            //登録
+            this.createTaskData();
 
+        } else if(this.operation == DB_OPERATION.READ ){
+            //表示
+            this.displayTaskData();
 
+        } else if(this.operation == DB_OPERATION.UPDATE ){
+            //編集
+            this.updateTaskData();
+
+        } else if(this.operation == DB_OPERATION.DELETE ){
+            //削除
+            this.deleteTaskData();
+
+        } else{
+            //do nothing
+        }
 
         return 0;
     }
 
     @Override
     protected void onPostExecute(Integer code) {
+
+    }
+
+
+    /*
+     * 「やること」の表示処理
+     */
+    private void displayTaskData(){
+
+
+    }
+
+    /*
+     * 「やること」の編集処理
+     */
+    private void updateTaskData(){
+
+    }
+
+
+    /*
+     * 「やること」の生成処理
+     */
+    private void createTaskData(){
+
+    }
+
+    /*
+     * 「やること」の削除処理
+     */
+    private void deleteTaskData(){
 
     }
 }
